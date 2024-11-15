7e3d22a7a25a07e96733b406d14ae496ef3b7d47
Rutger <de.kok.rutger@gmail.com>
2024-11-15 14:07:02 +0100

    customer-service and car-service updated SecurityConfig

--- car-service/src/main/java/dev/rutgerk/car_service/application/SecurityConfig.java
+++ car-service/src/main/java/dev/rutgerk/car_service/application/SecurityConfig.java
@@ -1,11 +1,29 @@
 package dev.rutgerk.car_service.application;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true) // Enable @PreAuthorize and other annotations
+@EnableWebSecurity
 public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+
+        // Define authorization rules for HTTP requests
+        .authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("*")
+            .permitAll().anyRequest().authenticated() // Secure all other requests
+        );
+
+    return http.build();
+  }
 
 }
