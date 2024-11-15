7eb08cee2e9253a97ba646c3048a3d80d4bf574d
Rutger <de.kok.rutger@gmail.com>
2024-11-15 13:32:27 +0100

    authentication-service missing @Component annotation added

--- authentication-service/src/main/java/dev/rutgerk/authentication_service/converter/RoleConverter.java
+++ authentication-service/src/main/java/dev/rutgerk/authentication_service/converter/RoleConverter.java
@@ -2,9 +2,11 @@ package dev.rutgerk.authentication_service.converter;
 
 import java.util.Set;
 import java.util.stream.Collectors;
+import org.springframework.stereotype.Component;
 import dev.rutgerk.authentication_service.dto.RoleDto;
 import dev.rutgerk.authentication_service.model.Role;
 
+@Component
 public class RoleConverter {
 
   public RoleDto convert(Role role) {
